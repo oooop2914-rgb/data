8ccca79a17addb99200ddd6c477be8235aa88022
oooop2914-rgb <oooop2914@gmail.com>
2026-03-11 12:32:45 +0700

    Create token.js

---
+++ token.js
@@ -0,0 +1,199 @@
+{
+  "owners": [
+    "6938178028"
+  ],
+  "config": {
+    "channel_id": "@",
+    "group_id": "-",
+    "group_link": ""
+  },
+  "tokens": [
+    "8600285858:AAE_1fHe-j5pWLn4cI79AKyZtG3fzFkjm-k",
+    {
+      "key": "REZZ-C058D9",
+      "expired": 1768834080649,
+      "created_by": 6938178028
+    },
+    "8384193061:AAFN1usaZ0-jfEKYHSpJhmE49RQ27WRzR30",
+    "gw",
+    "8600285858:AAE_1fHe-j5pWLn4cI79AKyZtG3fzFkjm-k",
+    "8572724458:AAFdFwTRNH_iIQW-ofHZkLp7_v1pufjAfDk",
+    "8007369739:AAFIFRqi7RxxhD7s2CdXGp0cHA3_eVgLrqU",
+    "8399100766:AAEmR9Sxp4zpraWhoU1nG5s-9IkuWRhS1iU",
+    "8495846718:AAG7INEMAwbf-_aiz6vw7o3iX2upl3MgUiA",
+    "8491878322:AAGw8j35X55Bxuux76MwmUx3TXXknAMX-do",
+    "8566625870:AAHhu-jSjNrAjNZzQ6QyU7FEBdNFBjTMfbc",
+    "8294180271:AAGJ6JK3pDp3yefOsEt5Rzc8Fx3mw-RnDQg",
+    "7939925628:AAHAP3ttPlsQkgAawwngnGRK_8fe-mH-jzk",
+    "8580861730:AAFgbmvJPL0j9qXajZbNMaw3__a8eTJvIvY",
+    "lenn",
+    "8109042174:AAF5nyyO85NsMym2VdRgEMOg1331odTdITA",
+    "8426974830:AAHjy9PU9wYzy4JrYVzwjYodwjVM1l6dNHI",
+    "8566625870:AAFzUUz3CRLnqORBER5pqWfbdXsM8OKu4_s",
+    "8109042174:AAHKrvReNqa4cNTkh6Cv2oQGUPTkcsI7c4s",
+    "8519230107:AAGc_LRQpbPtLTELhO4--_KRG4_TtmGd1Ug",
+    "7640555489:AAF9z-T-wf3eAUYz2l_UjzalZfrEVgnnY-k",
+    "8089260790:AAF0YFRksRJOsx4Tb0b7c9vZQrmq6iaNjvw",
+    "8325149717:AAHmqg_QuBeyCOig9YBSK0WPTdsWTdhxzio",
+    "8527349057:AAESrcmwIUqM87VgfKRCjEJi_1vFWQyO9Xw",
+    "8518179951:AAGfmAN6c7fZrlz1I95fHi3r7aRIheWc2Qo",
+    "8126849535:AAEjVE0C1UPmreSPXL6YGeKDIFFFbAaUcA4",
+    "8648400777:AAH3RsiPpVAOyeLRJ9Jyv54gmjAW02er108",
+    "8602124618:AAGfqq2-drpIJCeNzlZ3DgTIy-tZibuC36Q",
+    "8614242537:AAHyPDJvWKKPWkh1qR0jjswongAHhlPzHFI",
+    "8763857861:AAH_3s-Isns6vGhgY680_BIg1zOw9qwSJNQ"
+  ],
+  "sha": "b95ac927c57a768f2cb67e8ffc20b5506ac85f0d",
+  "list_buyer": [
+    {
+      "id": "6851921545",
+      "name": "Xven`zo 㕠",
+      "username": "Xvenzo",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "7726353539",
+      "name": "- z for?",
+      "username": "ZyyTsune",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "8064092635",
+      "name": "×͜× || 尺乇乙乙一緒",
+      "username": "ziistr",
+      "role": "ᴏᴡɴᴇƦ"
+    },
+    {
+      "id": "7476194064",
+      "name": "『𝗥𝟴』𝙇𝘼𝙉𝙂𝙕𝙕交",
+      "username": "Langzz31x",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "1619141907",
+      "name": "alwaysfazz (limit)",
+      "username": "fazzstudent",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "7720022578",
+      "name": "𝑅𝑒𝑧𝑖",
+      "username": "Alwaysrezz",
+      "role": "ƦᴇSSᴇʟᴇƦ"
+    },
+    {
+      "id": "7973092286",
+      "name": "⏤͟͟͞͞ARMUFA STORE⏤͟͟͞͞⸙ꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋꠋ",
+      "username": "MYARMUFASTORE",
+      "role": "ƦᴇSSᴇʟᴇƦ"
+    },
+    {
+      "id": "7785871986",
+      "name": "ALBERT",
+      "username": "Albertpurba29",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "6854552378",
+      "name": "斬 Zan | armufa",
+      "username": "armufastore1",
+      "role": "ƦᴇSSᴇʟᴇƦ"
+    },
+    {
+      "id": "7848477921",
+      "name": "¡𝗺 #𝐌𝐚𝐭𝐳",
+      "username": "xymatz",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "7822168842",
+      "name": "𝐉𝐨𝐨",
+      "username": "Jo_sk_yping2",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "8306155654",
+      "name": "⏤͟͟͞͞ 𝗧͢𝗿𝗶𝗽͢𝗹𝗲 𝗫 ™°|⏤͟͞𝗥𝟴|°™ZARR_HAMA ||私⏤͟͞𝗥𝟴",
+      "username": "No_Username",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "7622581857",
+      "name": "FlowraHanz",
+      "username": "FlowraHanz",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "8157817774",
+      "name": "𝐗𝐞𝐧 𝐎𝐟𝐟𝐜 [REHAT]",
+      "username": "xenoffc",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "5829437334",
+      "name": "Who?",
+      "username": "Lynuxyz",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "7655827848",
+      "name": "⏤͟͟͞͞X4R || Kazumii",
+      "username": "Kazumiiv1",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "7655827848",
+      "name": "⏤͟͟͞͞X4R || Kazumii",
+      "username": "Kazumiiv1",
+      "role": "ƦᴇSSᴇʟᴇƦ"
+    },
+    {
+      "id": "8512785930",
+      "name": "ㅤ",
+      "username": "lanz4npe",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "8317563450",
+      "name": "今 𝗫𝗜𝗗℻ | 夜 ¡m Aʅɯαყʂƙιxყȥ",
+      "username": "cskixyz",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "8529090104",
+      "name": "RAF_(²⁰²⁶)",
+      "username": "Raffa_md",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "7493384194",
+      "name": "SanzyHere",
+      "username": "LennSolzz",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "7153781494",
+      "name": "jung",
+      "username": "makanGhati",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "7843576576",
+      "name": "ARSTORE",
+      "username": "CsArstore",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "8798112790",
+      "name": "Vvip",
+      "username": "Vvip0_0",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    },
+    {
+      "id": "6764682875",
+      "name": "Hangz",
+      "username": "hananneedbuyer",
+      "role": "ᴍᴇᴍʙᴇƦ"
+    }
+  ]
+      }
